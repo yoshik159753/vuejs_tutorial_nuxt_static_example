7dde0074196a76163703c366a10851603ed3481b
YoshiK <kai.outsider.777+git@gmail.com>
2019-07-08 04:14:07 +0900

    Column Nuxt.js におけるサーバサイドレンダリング

--- pages/Product.vue
+++ pages/Product.vue
@@ -1,3 +1,28 @@
+<!--
+nuxt.js ではサーバサイドレンダリングを無効化することもできる。
+その場合には次のコマンドのように `--spa` を指定する。
+`npm run dev -- -spa`
+-->
+
 <template>
-  <h2>このページは Nuxt 社のプロダクト情報のページです。</h2>
+  <h2>{{ title }}</h2>
 </template>
+
+<script>
+export default {
+  // asyncData は Nuxt.js が提供する Vue.js のコンポーネントオプション。
+  // 非同期なデータを取得するためのメソッドで、 **取得したデータを返すことで Vue.js の data にマージされる** 。
+  async asyncData (context) {
+    return await new Promise(resolve => {
+      // バックエンドからのデータ取得をエミュレート
+      setTimeout(() => {
+        resolve({
+          title: `このページは Nuxt 社のプロダクト情報のページです。(${ process.server ? 'サーバサイドレンダリング' : 'クライアントサイドレンダリング' })`
+          // !!! 注) context.isServer は廃止されていた。今後は process.server !!!
+          // title: `このページは Nuxt 社のプロダクト情報のページです。(${ context.isServer ? 'サーバサイドレンダリング' : 'クライアントサイドレンダリング' })`
+        })
+      }, 1000)
+    })
+  }
+}
+</script>
